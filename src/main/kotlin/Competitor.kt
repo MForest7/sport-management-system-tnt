2d6221491c9100cc30e61915ef9e5e83eac57356
maksim <maximkhabarov@yandex.ru>
2021-11-20 22:57:03 +0300

    Adapted Competitor class for compatibility with other branches.

--- src/main/kotlin/Competitor.kt
+++ src/main/kotlin/Competitor.kt
@@ -1,19 +1,47 @@
 open class Competitor(
-    val name: String,
+    val wishGroup: String,
     val surname: String,
+    val name: String,
     val birth: String,
     val title: String,
-    val wishGroup: String,
     val MedicalExamination: String,
     val MedicalInsurance: String,
 ) {
     constructor(competitor: Competitor) : this(
-        competitor.name,
+        competitor.wishGroup,
         competitor.surname,
+        competitor.name,
         competitor.birth,
         competitor.title,
-        competitor.wishGroup,
         competitor.MedicalExamination,
         competitor.MedicalInsurance
     )
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Competitor
+
+        if (wishGroup != other.wishGroup) return false
+        if (surname != other.surname) return false
+        if (name != other.name) return false
+        if (birth != other.birth) return false
+        if (title != other.title) return false
+        if (MedicalExamination != other.MedicalExamination) return false
+        if (MedicalInsurance != other.MedicalInsurance) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = wishGroup.hashCode()
+        result = 31 * result + surname.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + birth.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + MedicalExamination.hashCode()
+        result = 31 * result + MedicalInsurance.hashCode()
+        return result
+    }
 }
