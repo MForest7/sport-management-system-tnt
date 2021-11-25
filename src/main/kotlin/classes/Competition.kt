1b49ec629bd07ac1ceed1a23c6864a4d8b093788
buyolitsez <hamerfall2003@yandex.ru>
2021-11-25 20:51:42 +0300

    Implemented readListOfCheckpointsFromDirectoryWithParticipantsResults method

--- src/main/kotlin/classes/Competition.kt
+++ src/main/kotlin/classes/Competition.kt
@@ -1,7 +1,25 @@
 package classes
 
+import parsers.IncompleteCheckpoint
+import parsers.readListOfIncompleteCheckpointsFromDirectoryWithParticipantsResults
+
 class Competition(
-    val checkpoints: List<CheckPoint>,
-    val competitors: List<CompetitorInCompetition>,
-    val numberMatching: Map<String, CompetitorInCompetition>
-)
+    private val checkpoints: List<CheckPoint>,
+    private val competitors: List<CompetitorInCompetition>,
+    private val numberMatching: Map<String, CompetitorInCompetition>
+) {
+    fun readListOfCheckpointsFromDirectoryWithParticipantsResults(dir: String): List<CheckPoint> {
+        val incompleteCheckpoints =
+            readListOfIncompleteCheckpointsFromDirectoryWithParticipantsResults(dir, checkpoints.map { it.name })
+
+        return incompleteCheckpoints.map { convertIncompleteCheckpointToCheckpoint(it) }
+    }
+
+    private fun convertIncompleteCheckpointToCheckpoint(incomplete: IncompleteCheckpoint): CheckPoint {
+        return CheckPoint(incomplete.name, incomplete.timeMatching.mapKeys {
+            val a = numberMatching[it.key]
+            require(a != null) { "Participant with number ${it.key} is undefined" }
+            a
+        })
+    }
+}
