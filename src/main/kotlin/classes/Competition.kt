5191a84ee1ee5c9124ce9605a15f2720a639bcd3
buyolitsez <hamerfall2003@yandex.ru>
2021-11-25 20:51:42 +0300

    Implemented readListOfCheckpointsFromDirectoryWithParticipantsResults method

--- src/main/kotlin/classes/Competition.kt
+++ src/main/kotlin/classes/Competition.kt
@@ -4,7 +4,4 @@ class Competition(
     val checkpoints: List<CheckPoint>,
     val competitors: List<CompetitorInCompetition>,
     val numberMatching: Map<String, CompetitorInCompetition>
-) {
-    val start = checkpoints.first()
-    val finish = checkpoints.last()
-}
+)
