59ba7fc63c3a8d72c0b4f8a3dff18d6a0dd1e962
maksim <maximkhabarov@yandex.ru>
2021-11-28 18:44:33 +0300

    Added printSortition

---
+++ src/main/kotlin/sortition/printSortition.kt
@@ -0,0 +1,39 @@
+package sortition
+
+import classes.Competition
+import classes.CompetitorInCompetition
+import classes.Group
+import com.github.doyaaaaaken.kotlincsv.client.ICsvFileWriter
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+
+fun groupToCompetitorMatching(competition: Competition): Map<Group, List<CompetitorInCompetition>> =
+    competition.competitors.groupBy { competitor -> competitor.group }
+
+fun writeCompetitor(csvFileWriter: ICsvFileWriter, competitor: CompetitorInCompetition, competition: Competition) {
+    val startTime = competition.checkpoints[0].timeMatching[competitor]
+    requireNotNull(startTime)
+    csvFileWriter.writeRow(
+        competitor.number, competitor.surname, competitor.name, competitor.birth,
+        competitor.title, startTime.stringRepresentation
+    )
+}
+
+fun writeCompetitorsInGroup(
+    dir: String,
+    group: Group,
+    competitors: List<CompetitorInCompetition>,
+    competition: Competition
+) {
+    csvWriter().open(dir + "/" + group.name) {
+        this.writeRow(listOf(group.name))
+        competitors.forEach { competitor ->
+            writeCompetitor(this, competitor, competition)
+        }
+    }
+}
+
+fun printSortitoin(dir: String, competition: Competition) {
+    groupToCompetitorMatching(competition).forEach { (group, competitors) ->
+        writeCompetitorsInGroup(dir, group, competitors, competition)
+    }
+}
