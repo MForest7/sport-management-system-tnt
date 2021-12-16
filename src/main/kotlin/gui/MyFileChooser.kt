b3b0ee1de38b6c32818abe74237e9dbb21335bd7
buyolitsez <hamerfall2003@yandex.ru>
2021-12-17 00:01:40 +0300

    Done reading config + errors catching

--- src/main/kotlin/gui/MyFileChooser.kt
+++ src/main/kotlin/gui/MyFileChooser.kt
@@ -5,14 +5,17 @@ import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileNameExtensionFilter
 
+class NoChosenFileException(message: String) : Exception(message)
+
 class MyFileChooser(private val window: ComposeWindow) {
     fun pickFile(extension: String = ""): String {
         val fc = JFileChooser()
         fc.currentDirectory = File(System.getProperty("user.dir"))
         fc.fileFilter = FileNameExtensionFilter("*.$extension", extension)
         fc.showDialog(window, "Attach")
-
-        require(fc.selectedFile != null) { "File was not chosen" }
+        if (fc.selectedFile == null) {
+            throw NoChosenFileException("File was not chosen")
+        }
         require(fc.selectedFile.absolutePath.endsWith(extension)) { "Wrong extension of file(expected $extension)" }
 
         return fc.selectedFile.absolutePath
