5ff97809c0776cd8d195b78558ace6788135a6f4
buyolitsez <hamerfall2003@yandex.ru>
2021-12-17 00:01:40 +0300

    Done reading config + errors catching

---
+++ src/main/kotlin/gui/ErrorDialog.kt
@@ -0,0 +1,54 @@
+package gui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import logger
+
+class MyErrorDialog {
+    private val openDialog = mutableStateOf(true)
+
+    @Composable
+    fun show(exception: Exception?) {
+        exception?.let {
+            logger.debug { "catch exception $exception" }
+
+            openDialog.value = true
+            alertDialog(it.message)
+        }
+    }
+
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    private fun alertDialog(message: String?) {
+        if (openDialog.value) {
+
+            AlertDialog(
+                modifier = Modifier.wrapContentSize(Alignment.Center).width(550.dp),
+                backgroundColor = Color.Red,
+                onDismissRequest = {
+                    openDialog.value = false
+                },
+                confirmButton = {
+                    Text(
+                        message ?: "something went wrong",
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            )
+        }
+    }
+}
+
+
--- src/main/kotlin/gui/sportManagerSystem.kt
+++ src/main/kotlin/gui/sportManagerSystem.kt
@@ -1,11 +1,14 @@
 package gui
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposeWindow
 import androidx.compose.ui.unit.dp
@@ -13,20 +16,70 @@ import logger
 import startShell
 
 @Composable
-@Preview
 fun sportManagerSystemApp(window: ComposeWindow) {
     logger.info { "App is started" }
 
     val myFileChooser = MyFileChooser(window)
+    val myErrorDialog = MyErrorDialog()
+    val exception = remember { mutableStateOf<Exception?>(null) }
 
-    MaterialTheme {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth().padding(8.dp)
+    ) {
         Button(onClick = {
-            startShell(myFileChooser.pickFile("json"))
-        }, modifier = Modifier.offset(x = 10.dp, y = 10.dp)) {
-            Text("Load config.json and start executing")
+            try {
+                startShell(myFileChooser.pickFile("json"))
+            } catch (e: Exception) {
+                exception.value = Exception(e.message)
+            }
+        }) {
+            Text("Load config")
         }
+
+
+        Button(onClick = {
+            try {
+                TODO()
+            } catch (e: Exception) {
+                exception.value = Exception(e.message)
+            }
+        }) {
+            Text("Information about participants")
+        }
+
+        Button(onClick = {
+            try {
+                TODO()
+            } catch (e: Exception) {
+                exception.value = Exception(e.message)
+            }
+        }) {
+            Text("Time of passing distances")
+        }
+
+        Button(onClick = {
+            try {
+                TODO()
+            } catch (e: Exception) {
+                exception.value = Exception(e.message)
+            }
+        }) {
+            Text("Results in groups")
+        }
+
+        Button(onClick = {
+            try {
+                TODO()
+            } catch (e: Exception) {
+                exception.value = Exception(e.message)
+            }
+        }) {
+            Text("Results in teams")
+        }
+
     }
 
-
+    myErrorDialog.show(exception.value)
     logger.info { "App is ended" }
 }
